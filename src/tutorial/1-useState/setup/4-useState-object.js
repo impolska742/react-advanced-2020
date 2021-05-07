5d323fc87568083d11a7a3cb5a18f241aac96ff0
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-05-07 16:18:41 +0530

    useState hooks with objects and ... spread operator

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const changeMessage = (message) => {
+    // setPerson({ ...person, message: "hello world" });
+    if (message != "hello world") {
+      setMessage("hello world");
+    } else {
+      setMessage("random message");
+    }
+  };
+
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => changeMessage(message)}
+      >
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
