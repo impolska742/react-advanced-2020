02d8428b6ebac9f707dd461b4757b83c72fbf655
Vaibhav Bhardwaj <vaibhav19bhardwaj@gmail.com>
2021-05-10 16:33:46 +0530

    useEffect short circuit files added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import Setup from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
   return (
     <div className="container">
-      <h2>Vaibhav Bhardwaj Here</h2>
+      {/* <h2>Vaibhav Bhardwaj Here</h2> */}
       <Setup />
     </div>
   );
